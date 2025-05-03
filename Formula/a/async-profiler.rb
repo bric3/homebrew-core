d0d77be36a7ec019ee0e01a8ea68ed6315c2a502
Brice Dutheil <brice.dutheil@gmail.com>
2025-05-03 11:17:08 +0200

    async-profiler 4.0 (build from source)

--- Formula/a/async-profiler.rb
+++ Formula/a/async-profiler.rb
@@ -1,34 +1,49 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class AsyncProfiler < Formula
-    desc "Sampling CPU & HEAP profiler for Java featuring AsyncGetCallTrace + perf_events"
-    homepage "https://github.com/jvm-profiling-tools/async-profiler"
+    desc "Sampling CPU and HEAP profiler for Java featuring AsyncGetCallTrace + perf_events"
+    homepage "https://github.com/async-profiler/async-profiler"
+    url "https://github.com/async-profiler/async-profiler/archive/refs/tags/v4.0.tar.gz"
+    version "4.0"
+    sha256 "7beb736868af485d6b0b624e42141f78df0ca8403188adc17965b7153261aa55"
     license "Apache-2.0"
-    on_linux do
-        on_intel do
-            url "https://github.com/async-profiler/async-profiler/releases/download/v4.0/async-profiler-4.0-linux-x64.tar.gz"
-            sha256 "ba32bd69cc2906f6251cd5aa8ae72c3abc12267ba858d38733f7751cc4d3e05b"
-        end
-
-        on_arm do
-            url "https://github.com/async-profiler/async-profiler/releases/download/v4.0/async-profiler-4.0-linux-arm64.tar.gz"
-            sha256 "482c142075dd8eeb762627087dd663725631e2747822f4480753e3b5507b3a62"
-        end
-    end
-
-    on_macos do
-        # The macOs distribution works for intel and arm64
-        url "https://github.com/async-profiler/async-profiler/releases/download/v4.0/async-profiler-4.0-macos.zip"
-        sha256 "1f27e5f5952ec40126a22addad6989bcbeb0509da8a2714ec489953abd698360"
-    end
-
+  
+    depends_on "cmake" => :build
+    depends_on "openjdk" => :test
+  
     def install
-        ohai "Installing #{HOMEBREW_PREFIX}/bin/asprof"
-        bin.install Dir["bin/*"]
-        ohai "Installing #{HOMEBREW_PREFIX}/lib/#{shared_library("libasyncProfiler")}"
-        lib.install Dir["lib/*"]
+      on_macos do
+        system "make", "FAT_BINARY=true"
+      end
+  #     on_linux do
+  #       system "make", "CC=/usr/local/musl/bin/musl-gcc"
+  #     end
+  
+      bin.install Dir["build/bin/*"]
+      lib.install Dir["build/lib/*"]
+      libexec.install Dir["build/jar/*"]
     end
-
+  
     test do
-        output = shell_output("#{bin}/asprof --version")
-        assert_match(/Async-profiler 4.0.+/, output.strip)
+      assert_match "Async-profiler #{version}", shell_output("#{bin}/asprof --version")
+  
+      (testpath/"Main.java").write <<~JAVA
+        public class Main {
+          public static void main(String[] args) throws Exception {
+            Thread.sleep(10_000);
+          }
+        }
+      JAVA
+  
+      pid = spawn Formula["openjdk"].bin/"java", testpath/"Main.java"
+      system bin/"asprof", "-d", "2", "-f", testpath/"test-profile-via-attach.html", "jps"
+      assert_predicate testpath/"test-profile-via-attach.html", :exist?
+  
+      system Formula["openjdk"].bin/"java", "-agentpath:#{lib}/libasyncProfiler.dylib=start,event=cpu,lock=10ms,file=test-profile-via-lib.jfr", testpath/"Main.java"
+      assert_predicate testpath/"test-profile-via-lib.jfr", :exist?
+    ensure
+      Process.kill("TERM", pid)
     end
-end
+  end
+  
