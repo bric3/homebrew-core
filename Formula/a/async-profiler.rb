71a760a9465814153713a1903c94add6e936c377
Brice Dutheil <brice.dutheil@gmail.com>
2025-05-03 21:40:51 +0200

    async-profiler 4.0 (build from source, head support)

--- Formula/a/async-profiler.rb
+++ Formula/a/async-profiler.rb
@@ -5,17 +5,18 @@ class AsyncProfiler < Formula
     version "4.0"
     sha256 "7beb736868af485d6b0b624e42141f78df0ca8403188adc17965b7153261aa55"
     license "Apache-2.0"
+    head "https://github.com/async-profiler/async-profiler.git", branch: "master"
   
     depends_on "cmake" => :build
     depends_on "openjdk" => :test
   
     def install
-      on_macos do
-        system "make", "FAT_BINARY=true"
-      end
-  #     on_linux do
-  #       system "make", "CC=/usr/local/musl/bin/musl-gcc"
-  #     end
+      args = []
+      args << "COMMIT_TAG=#{Utils.git_head}" if build.head?
+      args << "FAT_BINARY=true" if OS.mac?
+      # args << "CC=/usr/local/musl/bin/musl-gcc" if OS.linux?
+  
+      system "make", *args, "all"
   
       bin.install Dir["build/bin/*"]
       lib.install Dir["build/lib/*"]
@@ -28,7 +29,7 @@ class AsyncProfiler < Formula
       (testpath/"Main.java").write <<~JAVA
         public class Main {
           public static void main(String[] args) throws Exception {
-            Thread.sleep(10_000);
+            Thread.sleep(8_000);
           }
         }
       JAVA
@@ -39,6 +40,9 @@ class AsyncProfiler < Formula
   
       system Formula["openjdk"].bin/"java", "-agentpath:#{lib}/libasyncProfiler.dylib=start,event=cpu,lock=10ms,file=test-profile-via-lib.jfr", testpath/"Main.java"
       assert_predicate testpath/"test-profile-via-lib.jfr", :exist?
+  
+      system bin/"jfrconv", "-o", "pprof", testpath/"test-profile-via-lib.jfr", testpath/"test-profile-via-lib.pprof"
+      assert_predicate testpath/"test-profile-via-lib.pprof", :exist?
     ensure
       Process.kill("TERM", pid)
     end
