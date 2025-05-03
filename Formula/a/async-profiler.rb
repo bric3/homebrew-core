76332a3cc28540589609b32c1eac1fd42aa3a81b
Brice Dutheil <brice.dutheil@gmail.com>
2025-05-03 11:17:57 +0200

    async-profiler 4.0 (Removed template comments)

--- Formula/a/async-profiler.rb
+++ Formula/a/async-profiler.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class AsyncProfiler < Formula
     desc "Sampling CPU and HEAP profiler for Java featuring AsyncGetCallTrace + perf_events"
     homepage "https://github.com/async-profiler/async-profiler"
